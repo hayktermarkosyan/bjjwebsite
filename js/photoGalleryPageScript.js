424dac2861f26ed58930676a2c961f2716f36661
hayktermarkosyan <hayktermarkosyan@gmail.com>
2021-06-02 22:09:54 +0400

    Init commit

---
+++ js/photoGalleryPageScript.js
@@ -0,0 +1,9 @@
+const wow = new WOW({
+    boxClass: 'wow',
+    animateClass: 'animate__animated',
+    offset: 200,
+    mobile: true,
+    live: true
+});
+
+wow.init();
---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("menuBtn").addEventListener("click", function() {
+    document.getElementById("dropMenu").classList.toggle("openDropMenu");
+});
+
+document.getElementById("dropMenuItem1").addEventListener("click", function() {
+    document.getElementById("dropMenu").classList.toggle("openDropMenu");
+});
+
+document.getElementById("dropMenuItem2").addEventListener("click", function() {
+    document.getElementById("dropMenu").classList.toggle("openDropMenu");
+});
